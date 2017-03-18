96d0b68cc9fe5b6078ded212d7ffc1edefcd30fc
kittipong1121 <kittipong-k@hotmail.com>
2017-03-19 04:33:08 +0700

    สร้างหน้าผู้จัดทำเสร็จสิ้น

--- app/views/page3/page3.js
+++ app/views/page3/page3.js
@@ -1,3 +1,11 @@
-exports.pageloaded =(args) => {
+var frameModule = require("ui/frame");
+
+exports.pageloaded = (args) => {
     let page = args.object
 }
+
+exports.gohomepage = function() {
+    console.log('home complete!!!')
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/home/home");
+};
