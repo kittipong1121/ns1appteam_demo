27ee959e1e0a8e3a503405c45939a62e3a15b57d
kittipong1121 <kittipong-k@hotmail.com>
2017-03-19 03:38:46 +0700

    ปรับปรุงหน้าแสดงผล [หน้าแรก]

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,10 +1,23 @@
 var frameModule = require("ui/frame");
 
 exports.loaded = () => {
-    console.log('loaded cpmplete!!!')
+    //console.log('loaded cpmplete!!!')
 }
 
 exports.gologinpage = function() {
+    console.log('page1 cpmplete!!!')
     var topmost = frameModule.topmost();
-    topmost.navigate("views/login/login");
+    topmost.navigate("views/page1/page1");
+};
+
+exports.golistpage = function() {
+    console.log('page2 cpmplete!!!')
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/page2/page2");
+};
+
+exports.goaboutpage = function() {
+    console.log('page3 cpmplete!!!')
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/page3/page3");
 };
