a1ed1add8910553086918e7467566c803a915349
kittipong1121 <kittipong-k@hotmail.com>
2017-03-19 12:27:01 +0700

    ปรับปรุงหน้าข้อมูลผู้จัดทำ [page3]

--- platforms/android/src/main/assets/app/views/page3/page3.js
+++ platforms/android/src/main/assets/app/views/page3/page3.js
@@ -1,3 +1,4 @@
+var dialogs = require("ui/dialogs");
 var frameModule = require("ui/frame");
 
 exports.pageloaded = (args) => {
@@ -8,4 +9,14 @@ exports.gohomepage = function() {
     console.log('home complete!!!')
     var topmost = frameModule.topmost();
     topmost.navigate("views/home/home");
+};
+
+exports.detel = function() {
+    //---------- Show กล่องข้อความ -------------
+    dialogs.alert({
+            title: "รายละเอียด",
+            message: "Application นี้จัดทำเพื่อใช้ประกอบการอบรม การพัฒนาโปรแกรมบนอุปกรณ์เคลื่อนที่ด้วย Native Script วันที่ 15 - 19 มีนาคม 2560 โดยชมรมพัฒนาเทคโนโลยีเพื่อการ บริหารจัดการสารสนเทศบนระบบเครือข่าย",
+            okButtonText: "ปิด"
+        })
+        //---------- EnD Show กล่องข้อความ -------------        
 };
